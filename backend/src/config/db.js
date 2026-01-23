b5446cf72f2b03dc38552ac70b203641d5822c23
desarrollosnovonet <desarrollonvonet@novonet.net>
2026-01-23 01:21:32 -0500

    Activando SSL para base de datos Render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  // ESTA ES LA PARTE QUE TE FALTA PARA LA NUBE:
+  ssl: {
+    rejectUnauthorized: false // Esto permite la conexión segura con Render
+  }
 });
 
 module.exports = pool;

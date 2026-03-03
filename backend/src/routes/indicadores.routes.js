f8d46067e7ffeeca4a68dc165e3fa11d366bba34
desarrollosnovonet <desarrollonvonet@novonet.net>
2026-03-02 22:57:28 -0500

    agregamos routes

--- backend/src/routes/indicadores.routes.js
+++ backend/src/routes/indicadores.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
   getIndicadoresDashboard,
-  getMonitoreoDiario
+  getMonitoreoDiario,
+  getReporte180
 } = require('../controllers/indicadores.controller');
 
 router.get('/dashboard', getIndicadoresDashboard);
 router.get('/monitoreo-diario', getMonitoreoDiario);
+router.get('/reporte180', getReporte180);
 
 module.exports = router;

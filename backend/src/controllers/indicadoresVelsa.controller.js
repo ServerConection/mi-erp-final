93167e28dd8057cf84a3078d3fcf930ed214f2a8
desarrollosnovonet <desarrollonvonet@novonet.net>
2026-03-27 20:50:45 -0500

    Ingreso y arreglo de velsa

--- backend/src/controllers/indicadoresVelsa.controller.js
+++ backend/src/controllers/indicadoresVelsa.controller.js
@@ -193,7 +193,7 @@ const getIndicadoresDashboardVelsa = async (req, res) => {
                 vn.t2_fecha_activacion_telcos    AS "FECHA_ACTIVACION",
                 vn.t2_clausulas                  AS "NOVEDADES_ATC",
                 vn.t2_regularizado               AS "ESTADO_REGULARIZACION",
-                vn.t2_detalle_regularizado       AS "OBSERVACION_REGULARIZADO",
+                vn.t2_regularizado       AS "OBSERVACION_REGULARIZADO",
                 vn.t2_estado_regularizacion_novo AS "MOTIVO_REGULARIZAR",
                 vn.t2_observacion_auditoria      AS "OBSERVACION_AUDITORIA",
                 vn.t2_forma_pago                 AS "FORMA_PAGO",

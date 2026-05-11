dedb247541884f00b29988db43bc95f96d8da177
ServerConection <servidorender@novonet.net>
2026-05-11 14:07:55 -0500

    Fix: Usar indicadoresVelsaMaterialized.controller con rutas actualizadas

--- backend/src/routes/indicadoresVelsa.routes.js
+++ backend/src/routes/indicadoresVelsa.routes.js
@@ -6,13 +6,13 @@ const {
   getMonitoreoDiarioVelsa,
   getReporte180Velsa,
   getConsultaDescargaVelsa,
-  getDebugFechasVelsa,
-} = require('../controllers/indicadoresVelsa.controller');
+  getStatusMaterializedView,
+} = require('../controllers/indicadoresVelsaMaterialized.controller');
 
 router.get('/dashboard', getIndicadoresDashboardVelsa);
 router.get('/monitoreo-diario', getMonitoreoDiarioVelsa);
 router.get('/reporte180', getReporte180Velsa);
 router.get('/consulta-descarga', getConsultaDescargaVelsa);
-router.get('/debug-fechas', getDebugFechasVelsa);
+router.get('/status-mv', getStatusMaterializedView);
 
 module.exports = router;

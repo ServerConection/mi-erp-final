a82a3389e2700176f9d59e83211c08c2107763e1
ServerConection <servidorender@novonet.net>
2026-05-11 12:18:12 -0500

    Ingresos nuevos

---
+++ ersUsuario-PCoriginal_con_jotform.js
@@ -0,0 +1,42 @@
+[33m6ca1fd4[m Ingreso
+[33m5d075b8[m fix: null bytes indicadoresVelsa
+[33m841da51[m fix: migrar indicadoresVelsa a columnas Bitrix b_*/j_* (Cat.19 vista_8)
+[33mc68c941[m Correccion de conuslta de activas mes en velsa considerando las activas del mes con los ingresos del mes
+[33m55baa68[m Ingreso ventas correctasbacklog velsa
+[33m2dacf51[m Ingreso de indicadores
+[33m04d0d82[m Ingreso de nuevo pipeline
+[33m9c0c78e[m fix: usar parseFecha en t2_fecha_activacion_telcos del backlog para manejar todos los formatos de fecha
+[33mf58c360[m Ingreso
+[33m5bed7a1[m Ingreso
+[33mbc0a8ae[m Ingreso
+[33mbf578ac[m feat: zoom charts expandible, cuadrantes scatter, filtro JOT por campaña
+[33m66cc31c[m Fix: columna id_bitrix_ghl en consulta descarga Velsa
+[33m703e334[m Ingreso modulo descarga
+[33m3a1ecb5[m feat: login con ilustración animada + fix backlog query (filtro fecha creación)
+[33mf0a933b[m fix: backlog acumulado por rango de fechas + rediseño login telecomunicaciones
+[33mc5107c3[m perf: optimizar queries indicadores + fix supervisor reset, filtros manuales y redes
+[33m0dbd73e[m Ingreso de correcion de modulos efectividad
+[33m997a975[m feat: restaurar %ATC heatmap + filtro por campaña/origen en Indicadores y IndicadoresVelsa
+[33m93167e2[m Ingreso y arreglo de velsa
+[33m17a060b[m iNGRESO
+[33med8705a[m ingreso
+[33m4aabaaf[m Ingreso
+[33m5ef0d74[m Ingreso
+[33m1c0949c[m Ingreso
+[33m484346b[m Ingreso
+[33m718a27b[m Ingresi de const dedupVN
+[33m55eabac[m Ingreso correcto de backend para velsa indicadores
+[33mb2b73aa[m Ingreso correcto
+[33m4402608[m Ingreso Correcto
+[33mb9df699[m Ingreso gestionables velsa
+[33m58714f5[m Correcion
+[33m976a492[m Ingreso jot
+[33m9bbda10[m Nuevas modificaciones asesores
+[33m725c5fb[m ingreso
+[33m13abcbc[m Nuevos
+[33m2ae15e5[m neuva version
+[33m9121115[m Ingreso y actualiazacion de Velsa con backlog
+[33mf77a744[m Actualizacion de velsa completoa
+[33m317d266[m Ingreso de velsa
+[33m6537b4c[m descripción
+[33me080677[m feat: rutas indicadores velsa

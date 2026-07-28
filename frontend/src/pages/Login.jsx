7c932fd0d884388bf6d3d5003458710a396dc2d0
Bryan <bryanpinedasud@gmail.com>
2026-07-28 09:33:07 -0500

    Login: cambia imagen de fondo de PSC.gif a NET.png

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -102,7 +102,7 @@ export default function Login() {
         .lp-bg {
           position: absolute;
           inset: 0;
-          background-image: url('/PSC.gif');
+          background-image: url('/NET.png');
           background-size: cover;
           background-position: center center;
           background-repeat: no-repeat;
